eebb2e93f12bc237367cc9690f93a74bc740f9b5
Ekzzzzzz <mariadelosangeles505@hotmail.com>
2024-12-11 23:39:39 -0500

    Update Acerca_deController.java

--- src/main/java/Controladores/ControladorMenu/Bienvenida/Acerca_deController.java
+++ src/main/java/Controladores/ControladorMenu/Bienvenida/Acerca_deController.java
@@ -5,6 +5,5 @@ import Controladores.ControladorMenu.menu.NodeUpdator;
 public class Acerca_deController implements NodeUpdator {
     @Override
     public void updateNode() {
-        // nothing to update
     }
 }
